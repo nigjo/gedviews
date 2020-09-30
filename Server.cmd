2f44334f0f516e7babeaef4f922b8259d4eb9ddd
Jens Hofschröer <github@nigjo.de>
2020-09-30 20:03:42 +0200

    using Server.java from dedicated gist

--- Server.cmd
+++ Server.cmd
@@ -1,2 +1,2 @@
 @cd /d "%~dp0docs"
-@start "DebugServer" /WAIT /B cmd.exe /c "java ..\Server.java gedview || pause"
+@start "DebugServer" /WAIT /B cmd.exe /c "java ..\server\Server.java gedview || pause"
