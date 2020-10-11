f10cab8741ac9bb70de49d76dd0b49c2e560e098
Jens Hofschröer <github@nigjo.de>
2020-10-11 12:27:47 +0200

    Todays events are birth, death and marriages

--- docs/events/eventlist.js
+++ docs/events/eventlist.js
@@ -50,7 +50,7 @@ class GedEventsPage {
         }
         if (bdate.getDate() === today.getDate()
                 && bdate.getMonth() === today.getMonth()) {
-          events.today.push(indi);
+          events.today.push({type: 'BIRT', rec: indi});
         } else if (bdate.getTime() >= lastWeeks.getTime()
                 && bdate.getTime() < today.getTime()) {
           events.recent.push(indi);
@@ -60,7 +60,10 @@ class GedEventsPage {
         }
       }
       if (ddate) {
-        if (ddate.getTime() >= lastWeeks.getTime()
+        if (ddate.getDate() === today.getDate()
+                && ddate.getMonth() === today.getMonth()) {
+          events.today.push({type: 'DEAT', rec: indi});
+        } else if (ddate.getTime() >= lastWeeks.getTime()
                 && ddate.getTime() <= nextWeeks.getTime()) {
           events.deathdates.push(indi);
         }
@@ -68,55 +71,82 @@ class GedEventsPage {
       let marrdateRec = indi.getPath(["FAMS", "MARR", "DATE"]);
       if (marrdateRec) {
         let mdate = marrdateRec.getDayMonth();
-        if (mdate && mdate.getTime() >= lastWeeks.getTime()
-                && mdate.getTime() <= nextWeeks.getTime()) {
-          let fam = indi.getFamily();
-          if (events.marriages.indexOf(fam) < 0) {
-            events.marriages.push(indi.getFamily());
+        if (mdate) {
+          if (mdate.getDate() === today.getDate()
+                  && mdate.getMonth() === today.getMonth()) {
+            events.today.push({type: 'MARR', rec: indi.getFamily()});
+          } else if (mdate.getTime() >= lastWeeks.getTime()
+                  && mdate.getTime() <= nextWeeks.getTime()) {
+            let fam = indi.getFamily();
+            if (events.marriages.indexOf(fam) < 0) {
+              events.marriages.push(indi.getFamily());
+            }
           }
         }
       }
     }
     console.log(events);
 
-    this.printEvents("today", events.today, ["BIRT", "DATE"]);
+    this.printTodayEvents("today", events.today, true);
     this.printEvents("recent", events.recent, ["BIRT", "DATE"]);
     this.printEvents("next", events.next, ["BIRT", "DATE"]);
     this.printEvents("marriages", events.marriages, ["MARR", "DATE"]);
     this.printEvents("deathdates", events.deathdates, ["DEAT", "DATE"]);
   }
-  printEvents(type, indis, path)
+  printTodayEvents(groupid, items, showType = false)
   {
-    let div = document.getElementById(type);
+    let div = document.getElementById(groupid);
     while (div.firstChild)
       div.firstChild.remove();
-    if (indis.length > 0) {
-      indis.sort((a, b) => {
-        let delta = a.getPath(path).getDayMonth()
-                - b.getPath(path).getDayMonth();
+    if (items.length > 0) {
+      let knownIDs=[];
+      items = items.filter(item=>{
+        if(knownIDs.indexOf(item.rec.id)<0){
+          knownIDs.push(item.rec.id);
+          return true;
+        }
+        return false;
+      });
+      items.sort((item1, item2) => {
+        let delta = item1.rec.getPath([item1.type, "DATE"]).getDayMonth()
+                - item2.rec.getPath([item2.type, "DATE"]).getDayMonth();
         if (delta === 0) {
-          return a.getPath(path).getYear() - b.getPath(path).getYear();
+          return item1.rec.getPath([item1.type, "DATE"]).getYear()
+                  - item2.rec.getPath([item2.type, "DATE"]).getYear();
         }
         return delta;
       });
+
       let list = div.appendChild(document.createElement("ul"));
-      for (let indi of indis) {
-        let item = list.appendChild(document.createElement("li"));
-        item.classList.add("event");
-        let date = item.appendChild(document.createElement("span"));
+      for (let item of items) {
+        let listItem = list.appendChild(document.createElement("li"));
+        listItem.classList.add("event");
+        let date = listItem.appendChild(document.createElement("span"));
         date.classList.add("date");
-        date.append(indi.getPath(path).toLocaleString());
-        item.append(" ");
-        if (indi instanceof Individual) {
-          let name = item.appendChild(document.createElement("span"));
+        date.append(item.rec.getPath([item.type, "DATE"]).toLocaleString());
+        listItem.append(" ");
+        if (showType) {
+          let type = listItem.appendChild(document.createElement("span"));
+          switch(item.type){
+            case "BIRT":type.append("⚹");break;
+            case "DEAT":type.append("⚱");break;
+            case "MARR":type.append("⚭");break;
+            default:type.append("?");break;
+          }
+                  //.append(item.type);
+          listItem.append(" ");
+        }
+        if (item.rec instanceof Individual) {
+          let indi = item.rec;
+          let name = listItem.appendChild(document.createElement("span"));
           name.classList.add("name");
           name.append(indi.getIndiName());
           if (indi.getFirstSubRecord("FAMS")) {
             let allFams = indi.getSubRecords("FAMS");
-            for(let f=0;f<allFams.length;f++){
+            for (let f = 0; f < allFams.length; f++) {
               let fam = allFams[f];
-              item.append(" ");
-              let link = item.appendChild(document.createElement("a"));
+              listItem.append(" ");
+              let link = listItem.appendChild(document.createElement("a"));
               link.classList.add("fams");
               link.href = "?" + fam.toLocaleString().replace(/@/g, '');
               link.title = "Familie";
@@ -124,35 +154,48 @@ class GedEventsPage {
             }
           }
           if (indi.getFirstSubRecord("FAMC")) {
-            item.append(" ");
-            let link = item.appendChild(document.createElement("a"));
+            listItem.append(" ");
+            let link = listItem.appendChild(document.createElement("a"));
             link.classList.add("famc");
             link.href = "?" + indi.getFirstSubRecord("FAMC")
                     .toLocaleString().replace(/@/g, '');
             link.title = "Elternfamilie";
             link.append("🧒");
           }
-        } else if (indi instanceof Family) {
-          item.append(" ");
-          let husb = indi.getHusband();
-          let wife = indi.getWife();
-          let fname = item.appendChild(document.createElement("span"));
+        } else if (item.rec instanceof Family) {
+          let fam = item.rec;
+          listItem.append(" ");
+          let husb = fam.getHusband();
+          let wife = fam.getWife();
+          let fname = listItem.appendChild(document.createElement("span"));
           fname.classList.add("name");
           fname.appendChild(document.createElement("span"))
-            .textContent = (husb ? husb.getIndiName() : "???");
+                  .textContent = (husb ? husb.getIndiName() : "???");
           fname.append(" - ");
           fname.appendChild(document.createElement("span"))
-            .textContent = (wife ? wife.getIndiName() : "???");
-          item.append(" ");
-          let link = item.appendChild(document.createElement("a"));
+                  .textContent = (wife ? wife.getIndiName() : "???");
+          listItem.append(" ");
+          let link = listItem.appendChild(document.createElement("a"));
           link.classList.add("fams");
-          link.href = "?" + indi.id.replace(/@/g, '');
+          link.href = "?" + fam.id.replace(/@/g, '');
           link.title = "Familie";
           link.append("👫");
         }
       }
     } else {
       div.append("keine Ereignisse");
+  }
+  }
+  printEvents(type, indis, path)
+  {
+    if (indis.length > 0) {
+      let items = [];
+      for (let indi of indis) {
+        items.push({rec: indi, type: path[0]});
+      }
+      this.printTodayEvents(type, items);
+    } else {
+      this.printTodayEvents(type, []);
     }
   }
 }
