83dc96f59eb07ec03daa34abc9286905064cae2f
Jens Hofschröer <github@nigjo.de>
2020-11-01 23:05:15 +0100

    Plugin view should be localizable.

--- docs/simple/famview.js
+++ docs/simple/famview.js
@@ -13,116 +13,153 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+gvlocale.register("famview", null, {
+  title: "Familienansicht",
+  husb: "Vater",
+  wife: "Mutter",
+  granddad: "Großvater",
+  granny: "Großmutter",
+  num_child: "{0}. Kind",
+  testtext: "Das {1}. Kind ({0})",
+  born:"geboren",
+  died:"gestorben"
+});
+gvlocale.register("famview", "en", {
+  title: "Family Overview",
+  husb: "Father",
+  wife: "Mother",
+  granddad: "Grandfather",
+  granny: "Grandmother",
+  num_child: "Child #{0}",
+  born:"born",
+  died:"died"
+});
+
 class FamilyView {
 
-}
+  static LOGGER = "FamilyView";
 
-function printGedviewFamily(fam) {
-  console.log("Familie", fam);
-  let h1 = document.body.querySelector("header>h1");
-  let famtext =
-          (fam.getPath(["HUSB", "NAME", "SURN"]) || "").toLocaleString()
-          + " - " +
-          (fam.getPath(["WIFE", "NAME", "SURN"]) || "").toLocaleString();
-  if (famtext !== " - ") {
-    h1.append(": ", famtext);
-  }
-
-  let main = document.body.querySelector("main");
-
-  let husb = fam.getHusband();
-  if (husb) {
-    addIndi(husb, "parents", "Vater");
-    let famc = husb.getParentFamily();
-    if (famc) {
-      addIndi(famc.getHusband(), "grandparents", "Großvater", true);
-      addIndi(famc.getWife(), "grandparents", "Großmutter", true);
-    } else {
-      addIndi(false, "grandparents", "Großvater");
-      addIndi(false, "grandparents", "Großmutter");
+  getSurName(fam, type) {
+    if (fam) {
+      let surnRec = fam.getPath([type, "NAME", "SURN"]);
+      if (surnRec)
+        return surnRec.toLocaleString();
     }
+    return "";
   }
-  let wife = fam.getWife();
-  if (wife) {
-    addIndi(wife, "parents", "Mutter");
-    let famc = wife.getParentFamily();
-    if (famc) {
-      addIndi(famc.getHusband(), "grandparents", "Großvater", true);
-      addIndi(famc.getWife(), "grandparents", "Großmutter", true);
-    } else {
-      addIndi(false, "grandparents", "Großvater");
-      addIndi(false, "grandparents", "Großmutter");
+
+  printGedviewFamily(fam) {
+    console.log(FamilyView.LOGGER, fam);
+    let h1 = document.body.querySelector("header>h1");
+    let famtext =
+            this.getSurName(fam, "HUSB")
+            + " - " +
+            this.getSurName(fam, "WIFE");
+    if (famtext !== " - ") {
+      h1.append(": ", famtext);
     }
-  }
 
-  let CHILs = fam.getChildren();
-  for (let i = 0; i < CHILs.length; i++) {
-    addIndi(CHILs[i], "children", (i + 1) + ". Kind", true);
-  }
-}
+    let main = document.body.querySelector("main");
 
-function addIndi(indi, container, title, findOwnFamily = false) {
-  let div = document.getElementById("indi").content.cloneNode(true);
-  document.getElementById(container).append(div);
-  let child = document.getElementById(container).lastElementChild;
-  child.querySelector("h2").append(title);
-  if (indi) {
-    let allPaths = child.querySelectorAll("span[data-gedpath]");
-    for (var i = 0; i < allPaths.length; i++) {
-      let path = allPaths[i].dataset.gedpath.split('-');
-      let content = indi.getPath(path);
-      if (content) {
-        allPaths[i].append(content.toLocaleString());
+    let husb = fam.getHusband();
+    if (husb) {
+      this.addIndi(husb, "parents", gvlocale.get("famview.husb"));
+      let famc = husb.getParentFamily();
+      if (famc) {
+        this.addIndi(famc.getHusband(), "grandparents",
+                gvlocale.get("famview.granddad"), true);
+        this.addIndi(famc.getWife(), "grandparents",
+                gvlocale.get("famview.granny"), true);
+      } else {
+        this.addIndi(false, "grandparents", gvlocale.get("famview.granddad"));
+        this.addIndi(false, "grandparents", gvlocale.get("famview.granny"));
       }
     }
-    if (indi.getSubValue("FAMS", false)) {
-      let allFams = indi.getFamilies();
-      if (allFams.length === 1) {
-        if (findOwnFamily) {
+    let wife = fam.getWife();
+    if (wife) {
+      this.addIndi(wife, "parents", gvlocale.get("famview.wife"));
+      let famc = wife.getParentFamily();
+      if (famc) {
+        this.addIndi(famc.getHusband(), "grandparents", gvlocale.get("famview.granddad"), true);
+        this.addIndi(famc.getWife(), "grandparents", gvlocale.get("famview.granny"), true);
+      } else {
+        this.addIndi(false, "grandparents", gvlocale.get("famview.granddad"));
+        this.addIndi(false, "grandparents", gvlocale.get("famview.granny"));
+      }
+    }
+
+    let CHILs = fam.getChildren();
+    for (let i = 0; i < CHILs.length; i++) {
+      this.addIndi(CHILs[i], "children",
+              gvlocale.get("famview.num_child", i + 1), true);
+    }
+  }
+
+  addIndi(indi, container, title, findOwnFamily = false) {
+    let div = document.getElementById("indi").content.cloneNode(true);
+    document.getElementById(container).append(div);
+    let child = document.getElementById(container).lastElementChild;
+    child.querySelector("h2").append(title);
+    if (indi) {
+      let allPaths = child.querySelectorAll("span[data-gedpath]");
+      for (var i = 0; i < allPaths.length; i++) {
+        let path = allPaths[i].dataset.gedpath.split('-');
+        let content = indi.getPath(path);
+        if (content) {
+          allPaths[i].append(content.toLocaleString());
+        }
+      }
+      if (indi.getSubValue("FAMS", false)) {
+        let allFams = indi.getFamilies();
+        if (allFams.length === 1) {
+          if (findOwnFamily) {
+            child.classList.add("fams");
+            let famid = allFams[0];
+            child.addEventListener('click', function () {
+              switchFamily(famid);
+              return false;
+            });
+          }
+        } else {
           child.classList.add("fams");
-          let famid = allFams[0];
+          child.classList.add("moremarr");
           child.addEventListener('click', function () {
-            switchFamily(famid);
+            window.gedviewPage.openFamSelector(indi, allFams);
             return false;
           });
         }
-      } else {
-        child.classList.add("fams");
-        child.classList.add("moremarr");
-        child.addEventListener('click', function () {
-          openFamSelector(indi, allFams);
-          return false;
-        });
       }
     }
-  }
-  return child;
-}
-
-function openFamSelector(indi, allFams) {
-  let popup = document.createElement("div");
-  popup.classList.add("popup");
-  let closer = popup.appendChild(document.createElement("div"));
-  closer.classList.add("closer");
-  closer.textContent = "X";
-  closer.onclick = () => popup.remove();
-
-  popup.appendChild(document.createElement("div"))
-          .append("Bitte eine Familie wählen");
-
-  for (let fam of allFams) {
-    console.log(fam);
-    let row = popup.appendChild(document.createElement("div"));
-    row.classList.add("popuprow");
-    let link = document.createElement("a");
-    link.href = "?" + fam.id.replace(/@/g, "");
-    link.append(
-            (fam.getPath(["HUSB", "NAME"]) || "???").toLocaleString()
-            + " - " +
-            (fam.getPath(["WIFE", "NAME"]) || "???").toLocaleString());
-    row.appendChild(link);
+    return child;
   }
 
-  document.body.appendChild(popup);
+  openFamSelector(indi, allFams) {
+    let popup = document.createElement("div");
+    popup.classList.add("popup");
+    let closer = popup.appendChild(document.createElement("div"));
+    closer.classList.add("closer");
+    closer.textContent = "X";
+    closer.onclick = () => popup.remove();
+
+    popup.appendChild(document.createElement("div"))
+            .append("Bitte eine Familie wählen");
+
+    for (let fam of allFams) {
+      console.log(FamilyView.LOGGER, fam);
+      let row = popup.appendChild(document.createElement("div"));
+      row.classList.add("popuprow");
+      let link = document.createElement("a");
+      link.href = "?" + fam.id.replace(/@/g, "");
+      link.append(
+              (fam.getPath(["HUSB", "NAME"]) || "???").toLocaleString()
+              + " - " +
+              (fam.getPath(["WIFE", "NAME"]) || "???").toLocaleString());
+      row.appendChild(link);
+    }
+
+    document.body.appendChild(popup);
+  }
+
 }
 
+window.gedviewPage = new FamilyView();
